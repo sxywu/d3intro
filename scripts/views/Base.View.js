36d9de7783c6aa844788552eed4ade5a3f1d4b54
Shirley Wu <sxywu@Shirleys-MacBook-Pro.local>
2014-07-23 08:48:37 -0700

    delete all create code from BaseView

--- scripts/views/Base.View.js
+++ scripts/views/Base.View.js
@@ -24,8 +24,6 @@ define([
             'click .manipulateChild': 'manipulateChild',
             'keyup .bindData input': 'typeInput',
             'blur .bindData input': 'inputData',
-            'click .createEl': 'createEl',
-            'click .enterExit': 'enterExit',
             'click .runCode': 'runCode',
             'click .resetCode': 'resetCode'
         },
@@ -100,37 +98,6 @@ define([
             }
             this.toggleShowCode();
         },
-        createEl: function(e) {
-            var $createEl = ($(e.target).is('.createEl') ? $(e.target) : $(e.target).parents('.createEl'));
-            
-            if ($createEl.hasClass('active')) {
-                $createEl.removeClass('active');
-            } else {
-                this.$('.createEl.active').removeClass('active');
-                $createEl.addClass('active');
-            }
-            this.toggleShowCode();
-        },
-        enterExit: function(e) {
-            var $enterExit = ($(e.target).is('.enterExit') ? $(e.target) : $(e.target).parents('.enterExit'));
-            
-            if ($enterExit.hasClass('active')) {
-                $enterExit.removeClass('active');
-            } else {
-                this.$('.enterExit.active').removeClass('active');
-                $enterExit.addClass('active');
-
-                if (($enterExit.attr('data-type') === 'enter')
-                    || ($enterExit.attr('data-type') === 'update')) {
-                    this.$('.manipulateChild').addClass('hidden');
-                    $enterExit.next('.manipulateChild').removeClass('hidden');
-                } else {
-                    $enterExit.next('.manipulateChild').addClass('hidden');
-                }
-            }
-            
-            this.toggleShowCode();
-        },
         /* toggle show code button */
         toggleShowCode: function() {
             var demoHighlighted = this.$('.demoDiv.highlight').length,
@@ -155,19 +122,12 @@ define([
             var selection = this.selectionCode(),
                 data = this.bindDataCode(),
                 manipulate = this.manipulateCode(),
-                create = this.createElCode(),
-                enterExit = this.enterExitCode(),
                 code;
 
             if (selection) {
                 code = selection 
                     + data
                     + manipulate
-                    + ';';
-            } else if (create) {
-                code = create
-                    + data
-                    + enterExit
                     + ';';
             }
 
@@ -251,64 +211,19 @@ define([
             }
             return manipulate;
         },
-        createElCode: function() {
-            var create = '';
-
-            if (this.$('.createEl.active').length) {
-                var val = this.$('.createEl.active').attr('data-val');
-                create = 'd3.selectAll("' + val + '")';
-            }
-
-            return create;
-        },
-        enterExitCode: function() {
-            var enterExit = '',
-                type = this.$('.enterExit.active').attr('data-type'),
-                $child = this.$('.manipulateChild.active');
-
-            if (type === 'enter') {
-                var val = this.$('.createEl.active').attr('data-val');
-                enterExit = '\n  .enter().append("' + val + '")';
-
-                if ($child.length) {
-                    enterExit += '\n    .text(function(d) {return d;})';
-                }
-            } else if (type === 'update') {
-                if ($child.length) {
-                    enterExit += '\n  .text(function(d) {return d;})';
-                }
-            } else if (type === 'exit') {
-                enterExit = '\n  .exit().remove()';
-            }
-
-            return enterExit;
-        },
         runCode: function() {
-            // clear demo div's first
-            
             var code = this.$('pre').text(),
                 bindDataActivated = this.$('.bindData').length ?
-                    this.$('.bindData.active input').val() : false,
-                create = this.$('.createEl').length;
+                    this.$('.bindData.active input').val() : false;
 
-            if (create) {
-                code = code.replace('d3', 'd3.select("#' + this.id + ' .demoEnv .demoContainer")');
-            } else {
-                code = code.replace(/div/g, '#' + this.id + ' .demoDiv.highlight');
-            } 
+            code = code.replace(/div/g, '#' + this.id + ' .demoDiv.highlight'); 
 
             code = 'console.log(' + code.replace(/;/g, '') + ')';
             new Function(code)();
 
             if (bindDataActivated) {
-                if (create) {
-                    this.$('.demoContainer p').remove();
-                    var dataCode = this.createElCode()
-                        .replace('d3', 'd3.select("#' + this.id + ' .demoContainer")');
-                } else {
-                    this.$('.demoDiv p').remove();
-                    var dataCode = 'd3.selectAll("#' + this.id + ' .highlight")';
-                }
+                this.$('.demoDiv p').remove();
+                var dataCode = 'd3.selectAll("#' + this.id + ' .highlight")';
                 dataCode += this.bindDataCode();
                 dataCode += '.insert("p", "h4").text(function(d) {return "__data__: " + d;})'
 
